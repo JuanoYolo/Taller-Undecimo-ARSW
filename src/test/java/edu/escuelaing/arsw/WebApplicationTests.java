61db74feb45133b1af00936af13c181542ef8eb1
unknown <juan.monroy-s@labinfo.is.escuelaing.edu.co>
2022-07-19 08:52:04 -0500

    Primera pruebafuncionando

---
+++ src/test/java/edu/escuelaing/arsw/WebApplicationTests.java
@@ -0,0 +1,14 @@
+package edu.escuelaing.arsw;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class WebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
